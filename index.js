57f71eed7410845c6def1b556e56e09e44f8ce8d
Bryan Bishop <kanzure@gmail.com>
2013-08-12 12:22:02 -0500

    make a callable pipe for each downloadable file
    The returned dictionary now includes files that each have a property
    called "pipe" that can be given a writable stream.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var urlparser = require("url-parser");
 var request = require("request");
 var jsdom = require("jsdom");
 
+function makepipe(url) {
+    return function download(writestream) {
+        return request.get(url).pipe(writestream);
+    };
+};
+
 module.exports.test = function test(url) {
     parsedurl = urlparser.parse(url);
 
@@ -21,22 +27,47 @@ module.exports.test = function test(url) {
 };
 
 module.exports.download = function download(url, options, callback) {
-    request.get(url, function requestcallback(error, response, body) {
+    return request.get(url, function requestcallback(error, response, body) {
         var metadata = null;
 
         if (!error && response.statusCode == 200) {
             var window = jsdom.jsdom(body).createWindow();
             var title = window.document.getElementsByTagName("title")[0].innerHTML;
 
+            var meta_elements = window.document.evaluate("//meta[@name='citation_pdf_url']", window.document, null, 0, null);
+            var meta_pdf_url_element = meta_elements.iterateNext();
+
+            var pdfurl1 = null;
+            var pdfurl2 = null;
+
+            if (meta_pdf_url_element) {
+                pdfurl1 = meta_pdf_url_element.content;
+            } else {
+                pdfurl1 = "http://dx.plos.org/10.1371/journal.pone.0071630.pdf";
+            }
+
+            pdfurl2 = pdfurl1;
+
             metadata = {
                 "html": {
                     "title": title,
                 },
+
+                "files": [
+                    {
+                        "name": "paper.pdf",
+                        "url": pdfurl1,
+                        "pipe": makepipe(pdfurl1),
+                    },
+                    {
+                        "name": "supplementary.pdf",
+                        "url": pdfurl2,
+                        "pipe": makepipe(pdfurl2),
+                    },
+                ],
             };
         }
 
         callback(error, metadata);
     });
-
-    return undefined;
 };
