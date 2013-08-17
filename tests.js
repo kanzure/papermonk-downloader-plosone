d7898b4772aa426320cb9f76386b8a5284f4ac1c
Bryan Bishop <kanzure@gmail.com>
2013-08-17 02:36:29 -0500

    use trumpet for streaming html selectors
    Instead of passing in a callback, the download() method now returns a
    stream that receives "data" events with tiny fragments of metadata.
    
    This removes the dependency on cheerio.

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 var nock = require("nock");
 var test = require("tape");
-
-var cheerio = require("cheerio");
+var through = require("through");
+var trumpet = require("trumpet");
 
 test("require against the module", function(t) {
     var downloader = require("./");
@@ -70,25 +70,110 @@ test("extracts the correct html title", function(t) {
                 .replyWithFile(200, filepath);
 
     // make sure the title is extracted correctly
+    var paperstream = downloader.download("http://example.com/paper", {});
+
+    paperstream.on("data", function(data) {
+        if (data.title != undefined) {
+            t.equal(data.title, expected_title);
+        }
+
+        t.ok(scope.isDone(), "nock scope is done");
+        t.end();
+    });
+
+    paperstream.on("error", function(error) {
+        t.fail("error event was triggered");
+        t.end();
+    });
+
+    paperstream.on("end", function() {
+        t.end();
+    });
+
+    /*
     downloader.download("http://example.com/paper", {}, function(error, metadata, body) {
         t.equal(expected_title, metadata.html.title, "title matches expectations");
         t.ok(scope.isDone(), "nock scope is done");
         t.end();
-    });
+    }); */
 });
 
 test("parse title text correctly", function(t) {
-    var parse = require("./src/parse.js");
+    var parser = require("./src/parser.js");
     var expected_title = "hello world";
-    var $ = cheerio.load("<title>" + expected_title + "</title>");
-    t.equal(expected_title, parse.parseTitle($));
-    t.end();
+
+    var tr = trumpet();
+    var output = through();
+
+    // TODO: is this necessary?
+    tr.on("end", function() {
+        output.end();
+    });
+
+    // TODO: and this one..?
+    tr.on("error", function(error) {
+        output.emit("error", error);
+    });
+
+    // on to the testing!
+    output.on("data", function(data) {
+        if (data.hasOwnProperty("title")) {
+            t.equal(data.title, expected_title);
+        }
+    });
+
+    output.on("error", function(error) {
+        t.fail(error, "received the error signal");
+    });
+
+    output.on("end", function() {
+        t.end();
+    });
+
+    // setup the parser
+    parser.parseTitle(tr, output);
+
+    // test against some data
+    tr.write('<title>' + expected_title + '</title>');
+    tr.end();
 });
 
 test("parse citation_pdf_url correctly", function(t) {
-    var parse = require("./src/parse.js");
+    var parser = require("./src/parser.js");
     var expected_pdf_url = "http://example.com/example.pdf";
-    var $ = cheerio.load('<meta name="citation_pdf_url" content="' + expected_pdf_url + '" />');
-    t.equal(expected_pdf_url, parse.parsePdfUrl($));
-    t.end();
+
+    var tr = trumpet();
+    var output = through();
+
+    // TODO: is this necessary?
+    tr.on("end", function() {
+        output.end();
+    });
+
+    // TODO: and this one..?
+    tr.on("error", function(error) {
+        output.emit("error", error);
+    });
+
+    // on to the testing!
+    output.on("data", function(data) {
+        if (data.hasOwnProperty("pdfurl")) {
+            t.equal(data.pdfurl, expected_pdf_url);
+        }
+    });
+
+    output.on("error", function(error) {
+        t.fail(error, "received the error signal");
+    });
+
+    output.on("end", function() {
+        t.end();
+    });
+
+    // setup the parser to test
+    parser.parsePdfUrl(tr, output);
+
+    // test against some data
+    tr.write('<meta name="citation_pdf_url" content="' + expected_pdf_url + '" />');
+    tr.end();
 });
